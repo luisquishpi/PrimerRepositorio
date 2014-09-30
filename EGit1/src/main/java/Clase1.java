13592f477db6ed81fd38a08be26e2001c3162317
Luis Quishpi <AJT@138.4.205.235>
2014-09-30 13:58:48 +0200

    develop 3

--- EGit1/src/main/java/Clase1.java
+++ EGit1/src/main/java/Clase1.java
@@ -1,10 +1,11 @@
 
 public class Clase1 {
     private String mensaje;
+    private Clase3 clase3=new Clase3(5,6);
     public void setMensaje(String mensaje){
         this.mensaje=mensaje;
     }
     public String getMensaje(){
-        return this.mensaje;
+        return this.mensaje+clase3.suma();
     }
 }
