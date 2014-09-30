d60de2e5b8d30a9912f0731979987b8b9f0ca822
Luis Quishpi <AJT@138.4.205.235>
2014-09-30 13:56:18 +0200

    devolep 2

---
+++ EGit1/src/main/java/Clase3.java
@@ -0,0 +1,14 @@
+
+public class Clase3 {
+
+    int valo1=0;
+    int valor2=0;
+    public Clase3(int valor1, int valor2){
+        this.valo1=valor1;
+        this.valor2=valor2;
+    }
+    public int suma(){
+        return this.valo1+this.valor2;
+    }
+    
+}
