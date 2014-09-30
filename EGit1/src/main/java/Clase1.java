37f9dc2fc2aafec9785318a4e219c7724a19a79d
Luis Quishpi <AJT@138.4.205.235>
2014-09-30 13:48:33 +0200

    Nombre y Apellido

---
+++ EGit1/src/main/java/Clase1.java
@@ -0,0 +1,10 @@
+
+public class Clase1 {
+    private String mensaje;
+    public void setMensaje(String mensaje){
+        this.mensaje=mensaje;
+    }
+    public String getMensaje(){
+        return this.mensaje;
+    }
+}
